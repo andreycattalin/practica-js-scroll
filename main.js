0eea53102ca08ea6b35fed9f7db7904447e78fa9
Andrey <cattalin.htc@gmail.com>
2022-04-08 13:17:26 +0200

    --wip-- [skip ci]

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
-let totalHeight = document.body.offsetHeight - window.innerHeight ;
 
 function progressBar() {
+    let totalHeight = document.body.offsetHeight - window.innerHeight ;
+
     let scrollY = window.scrollY;
     let ancho = (scrollY * 100) / (totalHeight + 30);
 
